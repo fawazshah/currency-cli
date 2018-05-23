72f3f803db837eedc4490187eb61d6d4174a9405
fawazshah <fawaz010@gmail.com>
2018-05-23 12:31:19 +0100

    API access working

--- currency.js
+++ currency.js
@@ -1,5 +1,24 @@
 #!/usr/bin/env node
 
-var args = process.argv;
+var args = process.argv.slice(2);
+var http = require('http');
+
+var access_key = 'c8dc47fc727e256a4824b1885c7bd994';
 
 console.log(args);
+
+var latest_url = 'http://data.fixer.io/api/latest?access_key=' + access_key;
+
+http.get(latest_url, function(res) {
+  const { statusCode } = res;
+  let rawData = '';
+  res.on('data', (chunk) => { rawData += chunk; });
+  res.on('end', () => {
+    try {
+      const parsedData = JSON.parse(rawData);
+      console.log(parsedData);
+    } catch (e) {
+      console.error(e.message);
+    }
+  });
+});
