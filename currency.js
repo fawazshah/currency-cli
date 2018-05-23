53f5528f5bf78137f0ec9633aae3f899961b4f6a
fawazshah <fawaz010@gmail.com>
2018-05-23 18:59:40 +0100

    started big switch statement

--- currency.js
+++ currency.js
@@ -2,13 +2,30 @@
 
 var args = process.argv.slice(2);
 var http = require('http');
-
 var access_key = 'c8dc47fc727e256a4824b1885c7bd994';
+var error = 'Error';
 
-console.log(args);
+if (args.length != 0) {
+  switch (args[0]) {
+
+    case ('-l'):
+    case ('--latest'):
+      console.log('print latest value');
+      break;
+
+    case ('-c'):
+    case ('--convert'):
+      console.log('print converted values');
+      break;
+
+  }
+} else {
+  console.log(error);
+}
 
 var latest_url = 'http://data.fixer.io/api/latest?access_key=' + access_key;
 
+/*
 http.get(latest_url, function(res) {
   const { statusCode } = res;
   let rawData = '';
@@ -22,3 +39,4 @@ http.get(latest_url, function(res) {
     }
   });
 });
+*/
