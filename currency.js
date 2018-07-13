69826f93a8ff149f2816b36ef69b954146fe925b
fawazshah <fawaz010@gmail.com>
2018-07-14 00:28:39 +0100

    both 'latest' and 'convert' commands now accessible

--- currency.js
+++ currency.js
@@ -1,31 +1,58 @@
 #!/usr/bin/env node
 
-var args = process.argv.slice(2);
+var program = require('commander');
 var http = require('http');
-var access_key = 'c8dc47fc727e256a4824b1885c7bd994';
-var error = 'Error';
-
-if (args.length != 0) {
-  switch (args[0]) {
-
-    case ('-l'):
-    case ('--latest'):
-      console.log('print latest value');
-      break;
-
-    case ('-c'):
-    case ('--convert'):
-      console.log('print converted values');
-      break;
-
+var fs = require("fs");
+fs.readFile('access_key.txt', (err, data) => {
+  if (err) {
+    throw err;
+  } else {
+    var access_key = data;
   }
-} else {
-  console.log(error);
-}
+});
+var error = 'Error';
+ 
+program
+  .version('currency-cli version 0.1.0', '-v, --version');
 
-var latest_url = 'http://data.fixer.io/api/latest?access_key=' + access_key;
+program
+  .command('latest [currency]')
+  .description('get the latest currency value (base USD)')
+  .action(function (currency) {
+    console.log(currency);
+  });
+
+program
+  .command('convert [currency1] [currency2]')
+  .description('convert currency1 to currency2')
+  .action(function (currency1, currency2) {
+    console.log(currency1);
+    console.log(currency2);
+  });
+
+program.parse(process.argv);
+ 
+//if (args.length != 0) {
+//  switch (args[0]) {
+//
+//    case ('-l'):
+//    case ('--latest'):
+//      console.log('print latest value');
+//      break;
+//
+//    case ('-c'):
+//    case ('--convert'):
+//      console.log('print converted values');
+//      break;
+//
+//  }
+//} else {
+//  console.log(error);
+//}
 
 /*
+var latest_url = 'http://data.fixer.io/api/latest?access_key=' + access_key;
+
 http.get(latest_url, function(res) {
   const { statusCode } = res;
   let rawData = '';
