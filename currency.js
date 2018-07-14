f223969b86470c0214f9ed7b7fa3d7731e7c42c2
fawazshah <fawaz010@gmail.com>
2018-07-14 01:18:01 +0100

    added comments and error-checking for arguments

--- currency.js
+++ currency.js
@@ -3,6 +3,7 @@
 var program = require('commander');
 var http = require('http');
 var fs = require("fs");
+
 fs.readFile('access_key.txt', (err, data) => {
   if (err) {
     throw err;
@@ -10,24 +11,49 @@ fs.readFile('access_key.txt', (err, data) => {
     var access_key = data;
   }
 });
+
 var error = 'Error';
+
+//setting up each command-line command individually (using Commander API)
  
 program
   .version('currency-cli version 0.1.0', '-v, --version');
 
 program
   .command('latest [currency]')
-  .description('get the latest currency value (base USD)')
+  .description('get the latest value of the currency (with respect to USD)')
   .action(function (currency) {
+
+    if (!currency) {
+      console.log("No currency supplied!");
+      process.exit(1);
+    }
+
+    //TODO: check input currency belongs to list of valid currencies
+
     console.log(currency);
   });
 
 program
-  .command('convert [currency1] [currency2]')
-  .description('convert currency1 to currency2')
-  .action(function (currency1, currency2) {
+  .command('convert [amount] [currency1] [currency2]')
+  .description('convert amount of currency1 to currency2')
+  .action(function (amount, currency1, currency2) {
+
+    if (!amount || !currency1 || !currency2) {
+      console.log("Two currencies and an amount not supplied!");
+      process.exit(1);
+    }
+
+    if (typeof amount != "number") {
+      console.log("'amount' must be a number!");
+      process.exit(1);
+    }
+
+    //TODO: check input currencies belong to list of valid currencies
+
     console.log(currency1);
     console.log(currency2);
+
   });
 
 program.parse(process.argv);
