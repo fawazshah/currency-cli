a377d702e7660a7e99f1910c59a67636ae0a4b92
fawazshah <fawaz010@gmail.com>
2018-07-19 00:59:38 +0100

    'latest' command is working

--- currency.js
+++ currency.js
@@ -4,24 +4,32 @@ var commander = require('commander');
 var http      = require('http');
 var fs        = require("fs");
 
-var access_key = 'c8dc47fc727e256a4824b1885c7bd994';
-var endpointType;
+var accessKey = 'c8dc47fc727e256a4824b1885c7bd994';
+var latestUrl = 'http://data.fixer.io/api/latest?access_key=' + accessKey;
 
+/* only allow first argument if it is either -h or -v, or one of the 
+   preset commands */
+var firstArg = process.argv[2];
+if (!(firstArg == 'latest' || firstArg == 'convert'
+   || firstArg == '-h' || firstArg == '--help'
+   || firstArg == '-v' || firstArg == '--version')) {
+  console.error('No valid command supplied! See help menu (\'currency -h\')');
+  process.exit(1);
+}
+ 
 /* Each command-line command is set up individually (using 
    the Commander API). The help manual (accessed via -h and --help) 
    is set up automatically by Commander. We use standard processing
    of command-line arguments to deal with initial errors. */
 
-var firstArg = process.argv[2];
-if (!(firstArg == 'latest' || firstArg == 'convert'
-   || firstArg == '-h' || firstArg == '--help'
-   || firstArg == '-v' || firstArg == '--version')) {
-  console.log('No valid command supplied! See help menu (\'currency -h\')');
-  process.exit(1);
-}
- 
 commander
   .version('currency-cli version 0.1.0', '-v, --version');
+
+commander
+  .command('seeall')
+  .description('see all currencies and their 3 letter codes')
+  
+  //TODO: implement 'seeall' command
 
 commander
   .command('latest [currency]')
@@ -29,14 +37,38 @@ commander
   .action(function (currency) {
 
     if (!currency) {
-      console.log("No currency supplied!");
+      console.error("No currency supplied!");
       process.exit(1);
     }
 
-    // TODO: check input currency belongs to list of valid currencies
+    http.get(latestUrl, function(res) {
+      const { statusCode } = res;
+      let rawData = '';
+      res.on('data', (chunk) => { rawData += chunk; });
+      res.on('end', () => {
+        try {
 
-    endpointType = 'latest';
-    console.log(currency);
+          var json = JSON.parse(rawData);
+          var isInRates = false;
+          for (var key in json.rates) {
+            if (key == currency) {
+              isInRates = true;
+            }
+          }
+          if (isInRates) {
+            var relativeToUsd = 1 / json.rates[currency] * json.rates.USD;
+            console.log(relativeToUsd);
+          } else {
+            console.error("Not a valid currency! Currency must be supplied in its 3 letter all-caps form e.g. USD, GBP");
+            process.exit(1);
+          }
+
+        } catch (e) {
+          console.error(e.message);
+          process.exit(1);
+        }
+      });
+    });
   });
 
 commander
@@ -45,12 +77,12 @@ commander
   .action(function (amount, currency1, currency2) {
 
     if (!amount || !currency1 || !currency2) {
-      console.log("Two currencies and an amount not supplied!");
+      console.error("Two currencies and an amount not supplied!");
       process.exit(1);
     }
 
     if (typeof amount != "number") {
-      console.log("'amount' must be a number!");
+      console.error("'amount' must be a number!");
       process.exit(1);
     }
 
@@ -58,7 +90,8 @@ commander
 
     /* 'convert' endpoint type is not available for free, so we calculate 
        conversion with the 'latest' endpoint */
-    endpointType = 'latest';
+    var json = getJsonFromUrl(latestUrl);
+
     console.log(currency1);
     console.log(currency2);
 
@@ -66,26 +99,3 @@ commander
 
 commander
   .parse(process.argv);
- 
-var latest_url = 'http://data.fixer.io/api/latest?access_key=' + access_key;
-
-http.get(latest_url, function(res) {
-  const { statusCode } = res;
-  let rawData = '';
-  res.on('data', (chunk) => { rawData += chunk; });
-  res.on('end', () => {
-    try {
-
-      var parsedData = JSON.parse(rawData);
-      console.log(parsedData);
-      console.log(parsedData.rates.USD);
-
-    } catch (e) {
-      console.error(e.message);
-      process.exit(1);
-    }
-  });
-});
-
-var JSONObj = { "name": "hi" };
-console.log(JSONObj.name);
