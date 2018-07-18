af7687f3d975e7640ca50a55bd0fcd41a69c4497
fawazshah <fawaz010@gmail.com>
2018-07-18 23:36:28 +0100

    added more error-handling logic

--- currency.js
+++ currency.js
@@ -1,25 +1,29 @@
 #!/usr/bin/env node
 
-var program = require('commander');
-var http = require('http');
-var fs = require("fs");
+var commander = require('commander');
+var http      = require('http');
+var fs        = require("fs");
 
-fs.readFile('access_key.txt', (err, data) => {
-  if (err) {
-    throw err;
-  } else {
-    var access_key = data;
-  }
-});
+var access_key = 'c8dc47fc727e256a4824b1885c7bd994';
+var endpointType;
 
-var error = 'Error';
+/* Each command-line command is set up individually (using 
+   the Commander API). The help manual (accessed via -h and --help) 
+   is set up automatically by Commander. We use standard processing
+   of command-line arguments to deal with initial errors. */
 
-//setting up each command-line command individually (using Commander API)
+var firstArg = process.argv[2];
+if (!(firstArg == 'latest' || firstArg == 'convert'
+   || firstArg == '-h' || firstArg == '--help'
+   || firstArg == '-v' || firstArg == '--version')) {
+  console.log('No valid command supplied! See help menu (\'currency -h\')');
+  process.exit(1);
+}
  
-program
+commander
   .version('currency-cli version 0.1.0', '-v, --version');
 
-program
+commander
   .command('latest [currency]')
   .description('get the latest value of the currency (with respect to USD)')
   .action(function (currency) {
@@ -29,12 +33,13 @@ program
       process.exit(1);
     }
 
-    //TODO: check input currency belongs to list of valid currencies
+    // TODO: check input currency belongs to list of valid currencies
 
+    endpointType = 'latest';
     console.log(currency);
   });
 
-program
+commander
   .command('convert [amount] [currency1] [currency2]')
   .description('convert amount of currency1 to currency2')
   .action(function (amount, currency1, currency2) {
@@ -49,34 +54,19 @@ program
       process.exit(1);
     }
 
-    //TODO: check input currencies belong to list of valid currencies
+    // TODO: check input currencies belong to list of valid currencies
 
+    /* 'convert' endpoint type is not available for free, so we calculate 
+       conversion with the 'latest' endpoint */
+    endpointType = 'latest';
     console.log(currency1);
     console.log(currency2);
 
   });
 
-program.parse(process.argv);
+commander
+  .parse(process.argv);
  
-//if (args.length != 0) {
-//  switch (args[0]) {
-//
-//    case ('-l'):
-//    case ('--latest'):
-//      console.log('print latest value');
-//      break;
-//
-//    case ('-c'):
-//    case ('--convert'):
-//      console.log('print converted values');
-//      break;
-//
-//  }
-//} else {
-//  console.log(error);
-//}
-
-/*
 var latest_url = 'http://data.fixer.io/api/latest?access_key=' + access_key;
 
 http.get(latest_url, function(res) {
@@ -85,11 +75,17 @@ http.get(latest_url, function(res) {
   res.on('data', (chunk) => { rawData += chunk; });
   res.on('end', () => {
     try {
-      const parsedData = JSON.parse(rawData);
+
+      var parsedData = JSON.parse(rawData);
       console.log(parsedData);
+      console.log(parsedData.rates.USD);
+
     } catch (e) {
       console.error(e.message);
+      process.exit(1);
     }
   });
 });
-*/
+
+var JSONObj = { "name": "hi" };
+console.log(JSONObj.name);
